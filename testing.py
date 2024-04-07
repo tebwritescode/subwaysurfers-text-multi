c069a24908e0ae63c36f0cd61bd80990f7c96b05
ttai2023 <territai0831@gmail.com>
2024-04-06 20:36:28 -0700

    merge files

--- testing.py
+++ testing.py
@@ -1,7 +1,13 @@
-from videomaker import create_video
+from videomaker2 import create_video
+from vosk_example import get_words_and_timestamps
 
 # Array of strings to overlay at different timestamps
 texts = ['Text 1', 'Text 2', 'Text 3']
 timestamps = [2, 12, 25]  # timestamps
+
+INPUT_FILE = "output.wav"
+
+model_path = "/Users/danielbonkowsky/Documents/vosk-model-en-us-0.22"
+texts, timestamps = get_words_and_timestamps(model_path, INPUT_FILE)
 
 create_video(texts, timestamps, 10)
