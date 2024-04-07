97cc8def795a62a80949d662510fc2a2d817e26d
danielbonkowsky <danielbonkowsky@gmail.com>
2024-04-06 18:57:33 -0700

    comment edits

--- tts_example.py
+++ tts_example.py
@@ -13,12 +13,8 @@ text = article.cleaned_text
 
 voice = "en_us_006"
 
-# arguments:
-#   - input text
-#   - voice which is used for the audio
-#   - output file name
-#   - play sound after generating the audio
-tts(text, voice, "output.mp3", play_sound=False)
+#tts(input text, voice, output file, (optional) play sound)
+tts(text, voice, "output.mp3")
 
 sound = AudioSegment.from_mp3("output.mp3")
 sound.export("output.wav", format="wav")
