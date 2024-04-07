f415d23d1b69a66abc141f984c13682ee7633e4a
danielbonkowsky <danielbonkowsky@gmail.com>
2024-04-07 13:02:55 -0700

    Fixed chunking

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import os, re
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def get_file():
+    range_header = request.headers.get('Range', None)
+    byte1, byte2 = 0, None
+    if range_header:
+        match = re.search(r'(\d+)-(\d*)', range_header)
+        groups = match.groups()
+
+        if groups[0]:
+            byte1 = int(groups[0])
+        if groups[1]:
+            byte2 = int(groups[1])
+       
+    chunk, start, length, file_size = get_chunk(byte1, byte2)
+    resp = Response(chunk, 206, mimetype='video/mp4',
+                      content_type='video/mp4', direct_passthrough=True)
+    resp.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
+    return resp
+
 
 @app.route('/output', methods=['POST'])
 def output():
@@ -15,8 +32,32 @@ def output():
 def invalid_link():
     return render_template('invalid.html')
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Accept-Ranges', 'bytes')
+    return response
+
+
+def get_chunk(byte1=None, byte2=None):
+    full_path = "final.mp4"
+    file_size = os.stat(full_path).st_size
+    start = 0
+    
+    if byte1 < file_size:
+        start = byte1
+    if byte2:
+        length = byte2 + 1 - byte1
+    else:
+        length = file_size - start
+
+    with open(full_path, 'rb') as f:
+        f.seek(start)
+        chunk = f.read(length)
+    return chunk, start, length, file_size
+
+
 # this code runs the app in "development" mode which makes it easier when developing. 
 # comment out for final product
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, debug=True)
 
