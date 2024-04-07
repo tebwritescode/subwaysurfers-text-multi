ddd16827d656b8b6cd5f4f75f2665d1f29b96f24
gr125 <gaurirenjith5@gmail.com>
2024-04-07 12:57:45 -0700

    Revert "test chunking"
    This reverts commit 1df5d6b567c92ebfc1b374554af0b0787f56ee3f.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, Response, stream_with_context
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,36 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/output', methods=['GET','POST'])
+@app.route('/output', methods=['POST'])
 def output():
-    file_path = 'output.mp4'
-    file_size = os.path.getsize(file_path)
-    range_header = request.headers.get('Range', None)
-    
-    if not range_header:
-        # Directly return the file without chunking if no range header is present
-        with open(file_path, 'rb') as f:
-            return Response(f.read(), mimetype='video/mp4')
-
-    start, end = range_header.strip().lower().split('bytes=')[1].split('-')
-    start = int(start)
-    end = int(end) if end else file_size - 1
-    length_container = [end - start + 1] 
-
-    def generate_chunk():
-        with open(file_path, 'rb') as video_file:
-            video_file.seek(start)
-            chunk_size = 4096
-            while length_container[0] > 0:
-                chunk = video_file.read(min(chunk_size, length_container[0]))
-                if not chunk:
-                    break
-                yield chunk
-                length_container[0] -= len(chunk)
-
-    return Response(stream_with_context(generate_chunk()), status=206, mimetype='video/mp4',
-                    content_type='video/mp4', headers={'Content-Range': f'bytes {start}-{end}/{file_size}', 'Accept-Ranges': 'bytes'})
-
+    text_input = request.form['text_input']
+    return render_template('output.html', text_input=text_input)
 
 @app.route('/invalid_link')
 def invalid_link():
