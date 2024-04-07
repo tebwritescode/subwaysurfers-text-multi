1efafb3fc3a57b015d77eb43edf2ef2d662a0a60
danielbonkowsky <danielbonkowsky@gmail.com>
2024-04-07 14:17:28 -0700

    Compressed stuff

--- compression.py
+++ compression.py
@@ -3,5 +3,3 @@ from moviepy.editor import VideoFileClip
 def compress_video(input_path, output_path, bitrate='1000k'):
     clip = VideoFileClip(input_path)
     clip.write_videofile(output_path, bitrate=bitrate)
-
-compress_video('static/output.mp4', 'static/asdf.mp4', bitrate='1000k')
