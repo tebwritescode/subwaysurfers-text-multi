8dbc78e6b9ae93e681fb894ea34a6c7aa62cdea9
danielbonkowsky <danielbonkowsky@gmail.com>
2024-04-06 23:03:10 -0700

    Added ffmpeg audio concatting

--- testing.py
+++ testing.py
@@ -1,5 +1,7 @@
 from videomaker import create_video
 from vosk_example import get_words_and_timestamps
+import ffmpeg, os
+
 
 # Array of strings to overlay at different timestamps
 texts = ['Text 1', 'Text 2', 'Text 3']
@@ -10,4 +12,9 @@ INPUT_FILE = "output.wav"
 model_path = "/Users/danielbonkowsky/Documents/vosk-model-en-us-0.22"
 texts, timestamps = get_words_and_timestamps(model_path, INPUT_FILE)
 
-create_video(texts, timestamps, 30)
+create_video(texts, timestamps, 532)
+
+input_video = "output_video.mp4"
+input_audio = "output.wav"
+
+os.system("ffmpeg -i output_video.mp4 -i output.wav -c copy -map 0:v:0 -map 1:a:0 output.mp4 -shortest")
