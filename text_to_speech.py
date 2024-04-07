9b02f7bd4507dbc565d7702efc585f8a4d5b71a5
danielbonkowsky <danielbonkowsky@gmail.com>
2024-04-07 11:05:32 -0700

    Added voice choosing

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,15 +3,13 @@ from pydub import AudioSegment
 from goose3 import Goose
 import os, audioread
 
-VOICE = "en_us_006"
-
-def generate_wav(link):
+def generate_wav(link, voice):
     g = Goose()
     article = g.extract(link)
     text = article.cleaned_text
 
     #tts(input text, voice, output file, (optional) play sound)
-    tts(text, VOICE, "output.mp3")
+    tts(text, voice, "output.mp3")
 
     sound = AudioSegment.from_mp3("output.mp3")
     sound.export("output.wav", format="wav")
