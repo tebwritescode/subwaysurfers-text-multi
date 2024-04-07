c139aba7d0aca8599205ea9d0e7dbc7c57db1726
danielbonkowsky <danielbonkowsky@gmail.com>
2024-04-06 18:39:33 -0700

    minor edits

--- tts_example.py
+++ tts_example.py
@@ -1,14 +1,21 @@
 from tiktokvoice import tts
 from pydub import AudioSegment
-import os
+from goose3 import Goose
+import os, time
 
+link = input("Input link to article:: ")
 
-text = input("Input text to play:: ")
+start_time = time.time()
+
+g = Goose()
+article = g.extract(link)
+text = article.cleaned_text
+
 voice = "en_us_006"
 
 # arguments:
 #   - input text
-#   - vocie which is used for the audio
+#   - voice which is used for the audio
 #   - output file name
 #   - play sound after generating the audio
 tts(text, voice, "output.mp3", play_sound=False)
@@ -16,4 +23,5 @@ tts(text, voice, "output.mp3", play_sound=False)
 sound = AudioSegment.from_mp3("output.mp3")
 sound.export("output.wav", format="wav")
 
+print(f"Execution time: {time.time()-start_time} seconds")
 
--- vosk_example.py
+++ vosk_example.py
@@ -1,17 +1,23 @@
-import os, sys, wave, json
-
+import os, sys, wave, json, time
 from vosk import Model, KaldiRecognizer, SetLogLevel
+
+SENTENCE_LENGTH = 1
+INPUT_FILE = "output.wav"
+
+start_time = time.time()
 
 # Turn LOG messages on / off, 0 is on and -1 is off
 SetLogLevel(-1)
 
-# path to vosk model downloaded from
-# https://alphacephei.com/vosk/models
-# Currently using the 1.8Gb version
+""" 
+path to vosk model downloaded from 
+https://alphacephei.com/vosk/models
+Currently using the 1.8Gb version 
+"""
 model_path = "/Users/danielbonkowsky/Documents/vosk-model-en-us-0.22"
 
 # If model path doesn't work
-if not os.path.exists(model_path):
+if (not os.path.exists(model_path)):
     print(f"Please download the model from https://alphacephei.com/vosk/models and unpack as {model_path}")
     sys.exit()
 
@@ -19,22 +25,18 @@ if not os.path.exists(model_path):
 model = Model(model_path)
 
 # name of the audio file to recognize
-audio_filename = "output.wav"
-
-# name of the text file to write recognized text
-text_filename = "output.txt"
+audio_filename = INPUT_FILE
 
 # If audio file doesn't exist
-if not os.path.exists(audio_filename):
+if (not os.path.exists(audio_filename)):
     print(f"File '{audio_filename}' doesn't exist")
     sys.exit()
-
 
 # Reading the audio file
 wf = wave.open(audio_filename, "rb")
 
 # check if audio is mono wav
-if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+if (wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE"):
     print("Audio file must be WAV format mono PCM.")
     sys.exit()
 
@@ -44,7 +46,7 @@ rec.SetWords(True)
 results = []
 
 # recognize speech using vosk model
-while True:
+while (True):
     data = wf.readframes(4000)
     if len(data) == 0:
         break
@@ -59,7 +61,7 @@ words = []
 end_times = []
 
 for sentence in results:
-    if len(sentence) == 1:
+    if (len(sentence) == 1):
         # sometimes there are bugs in recognition 
         # and it returns an empty dictionary
         # {'text': ''}
@@ -70,3 +72,4 @@ for sentence in results:
 
 print(words)
 print(end_times)
+print(f"Execution time: {time.time()-start_time} seconds")
