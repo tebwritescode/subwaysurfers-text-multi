68445df737051398d22848e7283aab485ccc51eb
tebwritescode <93065214+tebwritescode@users.noreply.github.com>
2025-02-24 20:54:55 -0500

    Update concat.sh
    Added command replacements for using NVIDIA GPU

--- concat.sh
+++ concat.sh
@@ -1,2 +1,6 @@
 ffmpeg -i $1 -i $2 -c copy -map 0:v:0 -map 1:a:0 $3 -shortest
 ffmpeg -i $3 -vcodec h264 -acodec aac $4
+
+# Replace above commands with below commands to use NVIDIA GPU
+# ffmpeg -i "$1" -i "$2" -c:v h264_nvenc -preset slow -b:v 5M -c:a copy -map 0:v:0 -map 1:a:0 -shortest "$3"
+# ffmpeg -i "$3" -c:v h264_nvenc -c:a aac "$4"
