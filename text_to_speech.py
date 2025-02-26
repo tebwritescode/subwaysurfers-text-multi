3f0dd4cacfae75de3076c5c6c2d32400824f6836
tebwritescode <93065214+tebwritescode@users.noreply.github.com>
2025-02-25 22:48:00 -0500

    Update text_to_speech.py
    Added detection for text vs link

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,14 +2,21 @@ from tiktokvoice import tts
 from pydub import AudioSegment
 from goose3 import Goose
 import os, audioread
+import validators
 
-def generate_wav(link, voice, output_file):
-    g = Goose()
-    article = g.extract(link)
-    text = article.cleaned_text
-
-    #tts(input text, voice, output file, (optional) play sound)
+def generate_wav(article, voice, output_file):
+    # Check if the input is a URL
+    if validators.url(article):
+        # If it's a URL, extract text using Goose
+        g = Goose()
+        extracted_article = g.extract(article)
+        text = extracted_article.cleaned_text
+    else:
+        # If it's not a URL, assume it's direct text
+        text = article
+    
+    # Generate TTS and convert to WAV
     tts(text, voice, "output.mp3")
-
     sound = AudioSegment.from_mp3("output.mp3")
+
     sound.export(output_file, format="wav")
