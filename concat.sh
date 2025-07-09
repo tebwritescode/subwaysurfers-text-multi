b0e913dbfca52e341e43cf0dfe525c13fb5150e5
tebwritescode <93065214+tebwritescode@users.noreply.github.com>
2025-07-08 21:15:27 -0400

    Fix getting stuck on words, adjust video quality
    - Advance to the next word using frame indices to fix getting stuck on words
    - Added fast preset to ffmpeg command to increase completion speed

--- concat.sh
+++ concat.sh
@@ -1,4 +1,4 @@
-ffmpeg -i $1 -i $2 -c copy -map 0:v:0 -map 1:a:0 $3 -shortest
+ffmpeg -i $1 -i $2 -c copy -map 0:v:0 -map 1:a:0 $3 -shortest -preset fast
 ffmpeg -i $3 -vcodec h264 -acodec aac $4
 
 # Replace above commands with below commands to use NVIDIA GPU
--- videomaker.py
+++ videomaker.py
@@ -7,6 +7,7 @@ two lists, one of words and other of ending timestamps
 '''
 
 #video_length should be the length of the template video file, should be in seconds
+
 def create_video(words, timestamps, video_length, source_video, output_video):
     # loading video file
     video = cv2.VideoCapture(source_video)
@@ -23,30 +24,40 @@ def create_video(words, timestamps, video_length, source_video, output_video):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     output_video = cv2.VideoWriter(output_video, fourcc, fps, (width, height))
 
-    #Define the text properties
+    # Define the text properties
     font = cv2.FONT_HERSHEY_PLAIN
-    # text_position = (296, 640)
     font_scale = 3
     font_color = (255, 255, 255)
     thickness = 5
     outline_thickness = 10
 
-    # Loop through all the words/timestamps
-    framestamps = []
-    for time in timestamps:
-        framestamps.append(int(time * fps))
-    
+    # Convert timestamps to frame indices
+    framestamps = [int(time * fps) for time in timestamps]
+
+    # Defensive: Ensure framestamps and words are the same length
+    if len(framestamps) != len(words):
+        min_len = min(len(framestamps), len(words))
+        framestamps = framestamps[:min_len]
+        words = words[:min_len]
+
+    # Add a final frame index to mark the end of the last word
+    framestamps.append(video_length * fps)
+
     word_num = 0
     for i in range(video_length * fps):
-        if (i == framestamps[word_num] and word_num != len(words)-1):
+        # Advance word_num if we've reached the next framestamp
+        if word_num + 1 < len(framestamps) and i >= framestamps[word_num + 1]:
             word_num += 1
+        # Defensive: If word_num is out of bounds, break
+        if word_num >= len(words):
+            break
 
         ret, frame = video.read()
-        if (not ret):
+        if not ret:
             break
 
         word = words[word_num]
-        
+
         # Get the size of the text
         text_size = cv2.getTextSize(word, font, font_scale, thickness)[0]
 
